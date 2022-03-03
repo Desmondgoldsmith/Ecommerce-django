84615462bd095a78e0b52696ef752c7d51d7553f
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-03-03 13:11:01 +0000

    Added Home Templates [page] 😉

--- EcommerceApp/settings.py
+++ EcommerceApp/settings.py
@@ -116,7 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+   'EcommerceApp/static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
